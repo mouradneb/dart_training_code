15880685e15613e9fb88f25d311e35b9381ff66a
mouradneb <mourad.nebhi@gmail.com>
2021-06-05 22:22:01 +0100

    02 day

--- 08.8   for loop [ dart in Arabic ].dart
+++ 08.8   for loop [ dart in Arabic ].dart
@@ -1,1 +1,28 @@
-for_loop_08() {}
+for_loop_08() {
+  //for (var, condition, operator) { code }
+  for (int i = 0; i <= 10; i++) {
+    print(i);
+  }
+
+  for (int i = 0; i > 10; i++) {
+    print(i); // will not be run
+  }
+
+  print("");
+
+  for (int i = 20; i > 10; i--) {
+    print(i); // will  be run
+  }
+
+  print("");
+
+  for (String s in ["a", "b", "d"]) {
+    print(s);
+  }
+
+  print("");
+
+  for (String s in {"a", "b", "d"}) {
+    print(s);
+  }
+}
